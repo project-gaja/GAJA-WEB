e4219621934012373b8a3b0a2f595b1f316bb6f6
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-03-29 17:44:17 +0900

    디자인 변경

--- app/src/component/ScheduleCreation.jsx
+++ app/src/component/ScheduleCreation.jsx
@@ -1,7 +1,6 @@
 import { DateRange } from 'react-date-range';
-import { Component } from 'react';
 import Select from 'react-select'
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import './../styles/ScheduleCreation.css';
@@ -23,25 +22,28 @@ const ScheduleCreation = (props) => {
     { value: 'strawberry', label: 'Strawberry' },
     { value: 'vanilla', label: 'Vanilla' }
   ];
-  
 
   com.viewportset();  
+
+  let fn_crSchedule = () =>{
+    alert("값 : " + document.getElementById("name").value);
+    
+  };
   return (
     <div className='container'>
       <div className='box'>
-        <h4 className='title'>여행에 제목은 무엇인가요?</h4>
+        <h4 className='title'>😁여행에 제목은 무엇인가요?</h4>
         <input className='inputbox' type="text" id="name" name="name" required minlength="4" maxlength="8" ></input>
       </div>
          
       <div className='box'>
-        <h4 className='title'>여행에 제목은 무엇인가요?</h4>
+        <h4 className='title'>어디로🏖️ 여행을 떠나나요?</h4>
         <form >
-        <Select className='combox' options={options} />
-
+        <Select className='combox' id='combo' options={options} />
         </form>
       </div>
       <div className='box'>
-        <h4 className='title'>여행은 얼마나 떠나요?</h4>
+        <h4 className='title'>여행은 얼마나 떠나요?📅</h4>
         <DateRange
           editableDateInputs={true}
           onChange={item => setState([item.selection])}
@@ -50,7 +52,7 @@ const ScheduleCreation = (props) => {
           style={{ width: '100%' }}
         />
       </div>
-      <button className='button'>
+      <button className='button' onClick={fn_crSchedule}>
           여행 떠나기
       </button>
     </div>
