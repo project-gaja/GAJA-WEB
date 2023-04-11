2d13b21b110651690f794203afdc1abb00b0c951
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-04-12 02:24:33 +0900

    페이지별 사진업로드 다르게 되도록 수정

--- app/src/pages/Scheduling.jsx
+++ app/src/pages/Scheduling.jsx
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
@@ -7,87 +7,97 @@ import './../styles/Scheduling.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import axios from 'axios';
+import { API_LOCAL_URL } from './../common/constants';
 
 SwiperCore.use([Navigation, Pagination]);
 
 const Scheduling = () => {
   const location = useLocation();
   const data = location.state?.data;
-  const handleImageUpload = (event) => {
+  const [editorDataList, setEditorDataList] = useState(Array.from({ length: data }, () => '<p>일정을 등록해보세요😁</p>'));
+
+  const handleImageUpload = (event, index) => {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
-  
+
     // 이미지 업로드 API 호출
-    axios.post('이미지 업로드 API 주소', formData)
-      .then(response => {
+    axios
+      .post(API_LOCAL_URL + '/fileupload', formData)
+      .then((response) => {
         const imageUrl = response.data.imageUrl;
-        const editor = document.querySelector('.textedit .ck-editor__editable');
-        const imageElement = `<img src="${imageUrl}" alt="이미지" />`;
-        editor.insertAdjacentHTML('beforeend', imageElement);
+        const imageElement = `<img src='${API_LOCAL_URL + '/' + imageUrl}' alt='이미지' />`;
+        const newData = editorDataList.map((editorData, i) => (i === index ? editorData + imageElement : editorData));
+        setEditorDataList(newData);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
 
+  const handleEditorDataChange = (index, data) => {
+    const newData = editorDataList.map((editorData, i) => (i === index ? data : editorData));
+    setEditorDataList(newData);
+  };
+
   const slides = Array.from({ length: data }, (_, index) => (
     <SwiperSlide key={index}>
-      <div className='contain'>
-      <div className='test'>
-        <button className='coverbtn'>템플릿 변경</button>
-      </div>
-      <div className='topcon'>
-          <div className='topdiv'>
-            <p className='title'>우리 함께 떠나는 여행</p>
+      <div className="contain">
+        <div className="test">
+          <button className="coverbtn">템플릿 변경</button>
+        </div>
+        <div className="topcon">
+          <div className="topdiv">
+            <div className='top-left'>
+            <p className="title">우리 함께 떠나는 여행</p>
+            </div>
+            <div className='top-right'>
+            <label className='checkbox_con'>
+              <input type="checkbox" name="travel" />
+              공개
+            </label>  
+            </div>
           </div>
-          <div className='bottomdiv'>
-            <p className='title1'>{index + 1}-Day 일정</p>
-            <button className='locbtn'>위치추가</button>
-            <input type="file" id="imageInput" style={{ display: 'none' }} onChange={handleImageUpload} />
-            <button className='potobtn' onClick={() => document.getElementById('imageInput').click()}>사진추가</button>
+          <div className="bottomdiv">
+            <p className="title1">{index + 1}-Day 일정</p>
+            <button className="locbtn">위치추가</button>
+            <input type="file" name="image" id={`imageInput-${index}`} style={{ display: 'none' }} onChange={(event) => handleImageUpload(event, index)} />
+            <button className="potobtn" onClick={() => document.getElementById(`imageInput-${index}`).click()}>
+              사진추가
+            </button>
           </div>
-      </div>
-      <div textedit>
-        
-      </div>
-      <CKEditor className="textedit"
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!<img src='#'></p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />      
-      <div className='footer'>
-      <button className='addbtn'>일정등록</button>  
-      </div>
-      
+        </div>
+        <CKEditor
+          className="textedit"
+          id={`editor-${index}`}
+          editor={ClassicEditor}
+          data={editorDataList[index]}
+          onReady={(editor) => {
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            handleEditorDataChange(index, data);
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+        <div className="footer">
+          <button className="addbtn">일정등록</button>
+        </div>
       </div>
     </SwiperSlide>
   ));
 
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-    >
-      
+    <Swiper spaceBetween={50} slidesPerView={1} navigation pagination={{ clickable: true }}>
       {slides}
-      
     </Swiper>
   );
 };
+
 export default Scheduling;
