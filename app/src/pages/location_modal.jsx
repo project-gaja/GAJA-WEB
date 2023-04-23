09847a562068fa5e6c12fde4ea4968fe4c48790a
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-04-23 18:17:18 +0900

    geocoder 주석

--- app/src/pages/location_modal.jsx
+++ app/src/pages/location_modal.jsx
@@ -19,7 +19,7 @@ function ModalBasic({ setModalOpen, id, title, content, writer ,handleLocationAd
       //로케이션표시 Google maps에서 원하는 장소 찾은 후 주변검색을 누르면 좌표를 찾을 수 있다.
       const location = new naver.maps.LatLng(37.5663, 126.9779);
       
-      const geocoder = new naver.maps.services.Geocoder();
+      // const geocoder = new naver.maps.services.Geocoder();
       //네이버 지도 옵션 선택
       const mapOptions = {
         center: location,
